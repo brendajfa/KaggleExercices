b1c018a5b9c740dc1e94a3c7a218ae7f13127a9d
Brenda Fernandez <brendajfa@outlook.com>
2022-06-27 15:19:20 +0200

    commit.sh

--- commit.sh
+++ commit.sh
@@ -1,9 +1,6 @@
-
-
 git add --all;
-echo $0
-git commit -m $
-TOKEN = "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"
-USERNAME = "brendajfa"
-REPO = "KaggleExercices"
-git push https://$TOKEN@github.com/$USERNAME/$REPO.git
+git commit -m $0
+token_id="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"
+username="brendajfa"
+repo="KaggleExercices"
+git push https://$token_id@github.com/$username/$repo.git
